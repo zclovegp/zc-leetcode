1517f66820474aed4a736f41560985f238584e1f
zclovegp <1532458519@qq.com>
2022-10-07 21:01:46 +0800

    fix

---
+++ src/main/java/sort/FastSort.java
@@ -0,0 +1,57 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * @author zhaochong on 2022/10/6 22:34
+ */
+public class FastSort {
+
+	public static void main(String[] args) {
+		int[] targetArr = new int[]{6, 6, 2, 7, 9, 8, 4, 5, 10, 8};
+		fastSort(targetArr, 0, 9);
+		System.out.println(Arrays.toString(targetArr));
+	}
+
+	public static void fastSort(int[] targetArr, int staticLeft, int staticRight) {
+		if (staticLeft >= staticRight) {
+			return;
+		}
+
+		int targetIndex = staticLeft;
+		int targetValue = targetArr[targetIndex];
+		int left = staticLeft;
+		int right = staticRight;
+		String startFromLeftOrRight = "right";
+
+		// 一次快排结束
+		while (left < right) {
+			if (startFromLeftOrRight.equals("right")) {
+				if (targetValue <= targetArr[right]) {
+					right--;
+				} else {
+					switchLeftAndRight(targetArr, left, right);
+					startFromLeftOrRight = "left";
+					targetIndex = right;
+				}
+			} else {
+				if (targetValue >= targetArr[left]) {
+					left++;
+				} else {
+					switchLeftAndRight(targetArr, left, right);
+					startFromLeftOrRight = "right";
+					targetIndex = left;
+				}
+			}
+		}
+
+		fastSort(targetArr, staticLeft, targetIndex - 1);
+		fastSort(targetArr, targetIndex + 1, staticRight);
+	}
+
+	private static void switchLeftAndRight(int[] targetArr, int left, int right) {
+		int tmp = targetArr[left];
+		targetArr[left] = targetArr[right];
+		targetArr[right] = tmp;
+	}
+}
