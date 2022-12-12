47eefc091a3bf28618212c6fe1bb1269b03e4a06
zclovegp <1532458519@qq.com>
2022-12-12 09:04:00 +0800

    优化双指针找最长字符

--- src/main/java/双指针/MediumFindLongestWord.java
+++ src/main/java/双指针/MediumFindLongestWord.java
@@ -2,14 +2,19 @@ package 双指针;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 给你一个字符串 s 和一个字符串数组 dictionary ，找出并返回 dictionary 中最长的字符串，该字符串可以通过删除 s 中的某些字符得到。
+ * 524. 通过删除字母匹配到字典里最长单词
+ * 中等
+ * 322
+ * 相关企业
+ * 给你一个字符串 s 和一个字符串数组 dictionary ，找出并返回 dictionary 中最长的字符串，该字符串可以通过删除 s 中的某些字符得到。
  * <p>
  * 如果答案不止一个，返回长度最长且字母序最小的字符串。如果答案不存在，则返回空字符串。
  * <p>
- *  
+ * <p>
  * <p>
  * 示例 1：
  * <p>
@@ -19,7 +24,7 @@ import java.util.List;
  * <p>
  * 输入：s = "abpcplea", dictionary = ["a","b","c"]
  * 输出："a"
- *  
+ * <p>
  * <p>
  * 提示：
  * <p>
@@ -27,61 +32,76 @@ import java.util.List;
  * 1 <= dictionary.length <= 1000
  * 1 <= dictionary[i].length <= 1000
  * s 和 dictionary[i] 仅由小写英文字母组成
- * <p>
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode.cn/problems/longest-word-in-dictionary-through-deleting
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author zhaochong on 2022/8/24 21:58
  */
 public class MediumFindLongestWord {
 
 	public static void main(String[] args) {
-		System.out.println(findLongestWord("abce", ImmutableList.of("abc", "abe")));
+		System.out.println(findLongestWordV2("wordgoodgoodgoodbestword", ImmutableList.of("word", "good", "best", "good")));
 	}
 
-	public static String findLongestWord(String s, List<String> dictionary) {
+	public static String findLongestWordV2(String needDeleteStr, List<String> targetStrList) {
+		List<String> matchStrList = new ArrayList<>();
 
-		String maxItem = "";
+		for (String targetStr : targetStrList) {
+			char[] targetCharArr = targetStr.toCharArray();
+			char[] needDeleteCharArr = needDeleteStr.toCharArray();
 
-		for (String item : dictionary) {
+			int targetIndex = 0;
+			int needDeleteIndex = 0;
 
-			char[] waitDeleteArr = s.toCharArray();
-			char[] baseArr = item.toCharArray();
+			if (needDeleteCharArr.length < targetCharArr.length) {
+				continue;
+			}
 
-			int dIndex = 0;
-			int bIndex = 0;
+			while (needDeleteIndex < needDeleteCharArr.length && targetIndex < targetCharArr.length) {
 
-			// 遍历待删除字符串和比较字符串
-			while (true) {
-
-				// 包含base字符，更大的扔到maxItem中
-				if (bIndex >= baseArr.length) {
-					if (item.length() > maxItem.length()) {
-						maxItem = item;
-					}
-					// 长度相同，取字典序更低的
-					else if (item.length() == maxItem.length()) {
-						if (item.compareTo(maxItem) < 0) {
-							maxItem = item;
-						}
-					}
-					break;
+				// 字符相等，target扫描指针才向后移动
+				if (targetCharArr[targetIndex] == needDeleteCharArr[needDeleteIndex]) {
+					targetIndex++;
 				}
+				needDeleteIndex++;
 
-				// 不包含base字符
-				if (dIndex >= waitDeleteArr.length) {
-					break;
-				}
-
-				if (waitDeleteArr[dIndex] == baseArr[bIndex]) {
-					dIndex++;
-					bIndex++;
-				} else {
-					dIndex++;
+				if (targetIndex == targetCharArr.length) {
+					matchStrList.add(targetStr);
 				}
 			}
 		}
-		return maxItem;
+
+		if (matchStrList.size() == 0) {
+			return "";
+		}
+
+		// 选字符串长的
+		// 其次选字符小的
+		String result = "";
+		for (String resultStr : matchStrList) {
+			if (result.equals("")) {
+				result = resultStr;
+				continue;
+			}
+
+			// 长度优先
+			if (resultStr.length() > result.length()) {
+				result = resultStr;
+				continue;
+			}
+
+			// 字母小优先
+			if (resultStr.length() == result.length()) {
+				boolean enableReplace = false;
+				for (int i = 0; i < resultStr.length(); i++) {
+					if (resultStr.charAt(i) != result.charAt(i)) {
+						enableReplace = resultStr.charAt(i) < result.charAt(i);
+						break;
+					}
+				}
+				if (enableReplace) {
+					result = resultStr;
+				}
+			}
+		}
+		return result;
 	}
 }
