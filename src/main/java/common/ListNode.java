67417dc7d0c51c6c9616c3f6f473651f1c3a562f
zclovegp <1532458519@qq.com>
2023-07-31 11:23:41 +0800

    fix

---
+++ src/main/java/common/ListNode.java
@@ -0,0 +1,16 @@
+package common;
+
+/**
+ * @author zhaochong on 2023/7/31 11:22
+ */
+public class ListNode {
+
+	public int val;
+
+	public ListNode next;
+
+	public ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
--- src/main/java/双指针/寻找环形链表的首个节点.java
+++ src/main/java/双指针/寻找环形链表的首个节点.java
@@ -1,5 +1,7 @@
 package 双指针;
 
+import common.ListNode;
+
 /**
  * 快慢指针
  *
